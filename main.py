3ee982cf21e1b0786fcff899474c97e3b3431edd
dp0h <andrei.visnakovs@gmail.com>
2013-07-05 00:35:33 +0100

    Moved avg calculation to AverageMove class. Added tests for this class.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 from __future__ import print_function
 
 from collections import namedtuple
+from copy import deepcopy
 from datetime import datetime
 import numpy as np
 from marketdata import symbol, schema, update, access
@@ -28,13 +29,34 @@ def init_db(symbols, from_date, to_date):
     symbol.add_symbols(symbols)
     update.update_marketdata(from_date, to_date)
 
+MktTypeNames = ['open', 'high', 'low', 'close']
 
-MktDataItem = namedtuple('MktDataItem', ['date', 'open', 'high', 'low', 'close'])
+MktItem = namedtuple('MktItem', ['date'] + MktTypeNames)
+
+
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
+
+MktType = enum(*MktTypeNames)
+
+
+class AverageMove(object):
+    def __init__(self, size):
+        val = [(0, 0)] * size
+        self.__dict = {MktType.open: deepcopy(val), MktType.high: deepcopy(val),
+                       MktType.low: deepcopy(val), MktType.close: deepcopy(val)}
+
+    def add(self, type, idx, relative_val, val):
+        self.__dict[type][idx] = (self.__dict[type][idx][0] + float(val)/relative_val, self.__dict[type][idx][1] + 1)
+
+    def average(self, type):
+        return [acc/cnt for (acc, cnt) in self.__dict[type]]
 
 
 def to_talib_format(mdata):
     ''' Converts market daata to talib format '''
-    return MktDataItem(np.array([datetime(x.year, x.month, x.day) for x in mdata.index]), np.array([float(x) for x in mdata[0]]), np.array([float(x) for x in mdata[1]]), np.array([float(x) for x in mdata[2]]), np.array([float(x) for x in mdata[3]]))
+    return MktItem(np.array([datetime(x.year, x.month, x.day) for x in mdata.index]), np.array([float(x) for x in mdata[0]]), np.array([float(x) for x in mdata[1]]), np.array([float(x) for x in mdata[2]]), np.array([float(x) for x in mdata[3]]))
 
 
 def find_candlestick_patterns(dates, open, high, low, close):
@@ -49,6 +71,9 @@ def find_candlestick_patterns(dates, open, high, low, close):
             for idx, val in enumerate(res):
                 if val != 0:
                     print('%s: %s' % (str(dates[idx]).split(' ')[0], val))
+
+
+CONSIDERED_NDAYS = 10
 
 
 def main(fname, from_date, to_date):
@@ -66,39 +91,26 @@ def main(fname, from_date, to_date):
     #palg = [x for x in talib.get_functions() if 'CDL' in x]
     palg = ['CDLTHRUSTING']  # TEMP
     for a in palg:
-        # TODO: put aopen, ahigh, ..., to separate struct
-        cnt = 0
-        aopen = [0] * 10
-        ahigh = [0] * 10
-        alow = [0] * 10
-        aclose = [0] * 10
+        avg = AverageMove(CONSIDERED_NDAYS)
 
+        #TODO: different results should be consideres separateley, since we coldn't consider 100 and -100 in one avg.
         for s in symbols:
             f = getattr(talib, a)
             res = f(mdata[s].open, mdata[s].high, mdata[s].low, mdata[s].close)
             res = list(res)
-            #dates = [mdata[s][0][idx] for idx, val in enumerate(res) if val != 0]
             for x in (idx for idx, val in enumerate(res) if val != 0):
                 open = mdata[s].open[x]
-                cnt += 1
-                for i in range(10):
-                    aopen[i] += mdata[s].open[x + i]/open
-                    ahigh[i] += mdata[s].high[x + i]/open
-                    alow[i] += mdata[s].low[x + i]/open
-                    aclose[i] += mdata[s].close[x + i]/open
-        for x in range(10):
-            aopen[x] /= cnt
-            ahigh[x] /= cnt
-            alow[x] /= cnt
-            aclose[x] /= cnt
-        print(aopen)
-        print(ahigh)
-        print(alow)
-        print(aclose)
+                for i in range(CONSIDERED_NDAYS):
+                    avg.add(MktType.open, i, open, mdata[s].open[x + i])
+                    avg.add(MktType.high, i, open, mdata[s].high[x + i])
+                    avg.add(MktType.low, i, open, mdata[s].low[x + i])
+                    avg.add(MktType.close, i, open, mdata[s].close[x + i])
+        print(avg.average(MktType.open))
+        print(avg.average(MktType.high))
+        print(avg.average(MktType.low))
+        print(avg.average(MktType.close))
         # TODO: dates processing could be done using map-reduce, i.e. coungint average values
-    # TODO:
-    # Analize prices after event happens (for 10 days). Consider open and close prices.
-    # Show graph for average case
+    # Show graph for all cases average case
 
 
 if __name__ == '__main__':
