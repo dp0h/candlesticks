6f4b636ca94716f75b4e1ad40b53785db7fa131a
dp0h <andrei.visnakovs@gmail.com>
2013-07-04 02:09:24 +0100

    Modified algorithm to count changes after candlestick pattern event happens

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def check_db():
 def init_db(symbols, from_date, to_date):
     ''' Initializes marketdata db '''
     schema.create()
+    schema.disable_warnings()
     symbol.add_symbols(symbols)
     update.update_marketdata(from_date, to_date)
 
@@ -47,13 +48,49 @@ def find_candlestick_patterns(dates, open, high, low, close):
 
 
 def main(fname, from_date, to_date):
-    symbols = np.loadtxt(fname, dtype='S10', comments='#', skiprows=0)
+    # TODO: perhaps marketdata could be rewritten with use of MongoDb
+    #symbols = np.loadtxt(fname, dtype='S10', comments='#', skiprows=0)
+    symbols = ['BG.L']  # TEMP
     if not check_db():
         init_db(symbols, from_date, to_date)
+
+    # TODO: put all market data in separate class where we have logic to retrieve anything with lazy logic + caching
+    mdata = {}
     for s in symbols:
-        mdata = access.get_marketdata(s, from_date, to_date, [access.Column.Open, access.Column.High, access.Column.Low, access.Column.Close])
-        find_candlestick_patterns(*to_talib_format(mdata))
-        break  # remove
+        mdata[s] = to_talib_format(access.get_marketdata(s, from_date, to_date, [access.Column.Open, access.Column.High, access.Column.Low, access.Column.Close]))
+
+    #palg = [x for x in talib.get_functions() if 'CDL' in x]
+    palg = ['CDLTHRUSTING']  # TEMP
+    for a in palg:
+        cnt = 0
+        aopen = [0] * 10
+        ahigh = [0] * 10
+        alow = [0] * 10
+        aclose = [0] * 10
+
+        for s in symbols:
+            f = getattr(talib, a)
+            res = f(mdata[s][1], mdata[s][2], mdata[s][3], mdata[s][4])
+            res = list(res)
+            #dates = [mdata[s][0][idx] for idx, val in enumerate(res) if val != 0]
+            for x in (idx for idx, val in enumerate(res) if val != 0):
+                open = mdata[s][1][x]
+                cnt += 1
+                for i in range(10):
+                    aopen[i] = mdata[s][1][x + i]/open
+                    ahigh[i] = mdata[s][2][x + i]/open
+                    alow[i] = mdata[s][3][x + i]/open
+                    aclose[i] = mdata[s][4][x + i]/open
+        for x in range(10):
+            aopen[i] /= cnt
+            ahigh[i] /= cnt
+            alow[i] /= cnt
+            aclose[i] /= cnt
+        print(aopen)
+        print(ahigh)
+        print(alow)
+        print(aclose)
+        # TODO: dates processing could be done using map-reduce, i.e. coungint average values
     # TODO:
     # Analize prices after event happens (for 10 days). Consider open and close prices.
     # Show graph for average case
