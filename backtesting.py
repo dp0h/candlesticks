a78204acd8027728a7d6777b1b04e4fc795ab4cf
dp0h <andrei.visnakovs@gmail.com>
2013-08-12 00:10:16 +0100

    Added more implementation to backtesting.py

--- backtesting.py
+++ backtesting.py
@@ -5,7 +5,8 @@
 from __future__ import print_function
 
 from datetime import datetime
-from helpers import load_symbols, check_db, init_db, get_mkt_data
+from mktdata import init_marketdata, get_mkt_data
+from helpers import load_symbols, find_candlestick_patterns
 
 
 class StrategyRunner(object):
@@ -18,20 +19,29 @@ class StrategyRunner(object):
 
         self._balance = 0
 
+    def _process_event(mdata_idx, mdata):
+        start_price = mdata['open'][mdata_idx + 1] if mdata_idx + 1 < len(mdata['open']) else 0
+        end_price = mdata['open'][mdata_idx + 1 + self._hold_days] if mdata_idx + 1 + self._hold_days < len(mdata['open']) else 0
+        low_price = mdata['low'][mdata_idx + 1:mdata_idx + 1 + self._hold_days]
+        high_price = mdata['high'][mdata_idx + 1:mdata_idx + 1 + self._hold_days]
+        if (self._side == 'buy'):
+            self._process_buy()
+        else:
+            self._process_sell()
+
     def __call__(self, symbols, from_date, to_date):
-        for s in self.__symbols:
+        for s in symbols:
             mdata = get_mkt_data(s, from_date, to_date)
             for a in self.__palg:
                 res = find_candlestick_patterns(a, mdata)
-                pass
-                #self._process_patterns(res, mdata, a)
+                for (idx, val) in res:
+                    self._process_event(idx, mdata)
         return self
 
 
 def main(fname, from_date, to_date):
     symbols = load_symbols(fname)
-    if not check_db():
-        init_db(symbols, from_date, to_date)
+    init_marketdata(symbols, from_date, to_date)
 
     strategy_inputs = [
         ('CDL3LINESTRIKE', -100, 'buy', 10, 1.5),
